36ced6530657b240820a0297c16ba375b3ab5649
ratnapriya2002 <chamalapriya@gmail.com>
2023-03-01 05:19:33 +0000

    SupeOve Leagug

---
+++ index.js
@@ -0,0 +1,18 @@
+//   Write your code here.
+const element = (
+  <div className="super-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="teams-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="logo-image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="logo-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
